2ef6db4d5c6516bc0085f280d45a7f2e8ffe54c2
Mukesh Kumar <thappamkkumar@gmail.com>
2025-12-27 00:38:33 +0530

    update

--- src/content/footerData.ts
+++ src/content/footerData.ts
@@ -1,8 +1,9 @@
 import type { FooterData, FooterHeading } from "@/types/footer";
+import {businessName} from './siteConfig';
 
 export const footerData: FooterData = {
     copyright:
-      "© 2025 Your Business Name. All rights reserved.",
+      "© 2025 "+ businessName +". All rights reserved.",
     socialLinks: [
       { label: "Instagram", href: "#" },
       { label: "Facebook", href: "#" },
